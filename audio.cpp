554a2ac5ab2d5405d57beb96268af7a92fab8f7b
Curtis Malainey <curtis@malainey.com>
2017-03-17 22:34:10 -0600

    dump max fft

--- audio.cpp
+++ audio.cpp
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <bass.h>
 #include <bassmix.h>
+#include <stdlib.h>
+#include <math.h>
 
 #define SPECWIDTH 64 // display width (should be multiple of 4)
 #define SPECHEIGHT 32  // height (changing requires palette adjustments too)
 #define BANDS 64
-
+#define MAX(x,y) ((x > y) ? x : y)
 HRECORD line_in;
 HSTREAM line_out;
 
@@ -33,8 +35,19 @@ int main(int argc, char *argv[])
   }
 
   BASS_PluginLoad("libbass_aac.so",0); // load BASS_AAC (if present) for AAC support
-
-  while(1){}
+  float fft[1024];
+  while(1)
+  {
+    int x, y, y_max;
+    y_max = 0;
+    BASS_ChannelGetData(chan,fft,BASS_DATA_FFT2048); // get the FFT data
+    for (x=0;x<SPECWIDTH*2;x+=2) {
+      y=sqrt(fft[x+1])*3*SPECHEIGHT-4; // scale it (sqrt to make low values more visible)
+      y_max = MAX(y, y_max);
+    }
+    fprintf(stderr, "FFT: %d\n", y_max);
+    usleep(refresh_rate * 1000);
+  }
 
   BASS_RecordFree();
   BASS_Free();
