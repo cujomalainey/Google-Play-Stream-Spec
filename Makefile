7d202e98958e83611ef33fd9be133850e16cf1d0
Curtis Malainey <curtis@malainey.com>
2018-01-01 20:14:14 -0700

    Add socket

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ CXXFLAGS=-Wall -O3 -g -fno-strict-aliasing
 COMMON_LIBS=-lstdc++
 
 audio: audio.cpp
-	gcc audio.cpp -lm -lbass -lbassmix $(COMMON_LIBS) $(CXXFLAGS) -o audio
+	gcc audio.cpp -lm -lbass -L. $(COMMON_LIBS) $(CXXFLAGS) -o audio
 
 matrix: matrix.cpp
 	gcc main.cpp -lrgbmatrix $(COMMON_LIBS) $(CXXFLAGS) -o matrix
--- audio.cpp
+++ audio.cpp
@@ -1,19 +1,37 @@
 #include <stdio.h>
-#include <bass.h>
-#include <bassmix.h>
+#include "bass.h"
 #include <stdlib.h>
 #include <math.h>
 #include <unistd.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
 
 #define SPECWIDTH 64 // display width (should be multiple of 4)
 #define SPECHEIGHT 32  // height (changing requires palette adjustments too)
 #define BANDS 64
+#define SERVER "192.168.0.32"
+#define BUFLEN 144  //Max length of buffer
+#define PORT 8888   //The port on which to send data
 #define MAX(x,y) ((x > y) ? x : y)
+
 HRECORD line_in;
 HSTREAM line_out;
 uint16_t refresh_rate = 5;
+
+void die(char *s)
+{
+    perror(s);
+    exit(1);
+}
+
 int main(int argc, char *argv[])
 {
+  struct sockaddr_in si_other;
+  int s, i, slen=sizeof(si_other);
+  char buf[BUFLEN];
+  char message[BUFLEN];
+  float fft[1024];
   // initialize BASS
   if (!BASS_Init(-1,44100,0,NULL,NULL) || !BASS_RecordInit(-1))
   {
@@ -22,21 +40,28 @@ int main(int argc, char *argv[])
   }
   BASS_SetConfig(BASS_CONFIG_UPDATETHREADS, 3);
 
+  if ( (s=socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1)
+  {
+      die("socket");
+  }
+
+  memset((char *) &si_other, 0, sizeof(si_other));
+  si_other.sin_family = AF_INET;
+  si_other.sin_port = htons(PORT);
+
+  if (inet_aton(SERVER , &si_other.sin_addr) == 0)
+  {
+      fprintf(stderr, "inet_aton() failed\n");
+      exit(1);
+  }
+
   line_in = BASS_RecordStart(44100, 2, 0, NULL, NULL);
-  line_out = BASS_Mixer_StreamCreate(44100, 2, 0);
 
-  if (!BASS_Mixer_StreamAddChannel(line_out, line_in, 0))
+  if (!BASS_RecordStart(48000, 1, 0, NULL, NULL))
   {
-    printf("Can't patch channels\n");
+    printf("Can't record channel\n");
   }
 
-  if (!BASS_ChannelPlay(line_out, false))
-  {
-    printf("Can't play channel\n");
-  }
-
-  BASS_PluginLoad("libbass_aac.so",0); // load BASS_AAC (if present) for AAC support
-  float fft[1024];
   while(1)
   {
     int x, y, y_max;
@@ -47,10 +72,16 @@ int main(int argc, char *argv[])
       y_max = MAX(y, y_max);
     }
     fprintf(stderr, "FFT: %d\n", y_max);
+    //send the message
+    if (sendto(s, message, strlen(message) , 0 , (struct sockaddr *) &si_other, slen)==-1)
+    {
+        die("sendto()");
+    }
     usleep(refresh_rate * 1000);
   }
 
   BASS_RecordFree();
   BASS_Free();
+  close(s);
   return 0;
 }
